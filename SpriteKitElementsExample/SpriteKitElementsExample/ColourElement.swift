a667afd97d3fcaee6af96f59c9db1ad3a26143b9
Nicholas Cross <nicholas@nacrossweb.com>
2015-04-16 22:37:53 +1000

    Simplified example element by using hue instead of rgb to change colours.  This also looks more vibrant.

--- SpriteKitElementsExample/SpriteKitElementsExample/ColourElement.swift
+++ SpriteKitElementsExample/SpriteKitElementsExample/ColourElement.swift
@@ -11,40 +11,26 @@ import SpriteKit
 
 @objc class ColourElement : SpriteElement {
     
-    let red = SpriteEssence<CGFloat>()
-    let green = SpriteEssence<CGFloat>()
-    let blue = SpriteEssence<CGFloat>()
+    let hue = SpriteEssence<CGFloat>()
     
     func didAttach(node: SKNode, inScene scene: SpriteElementScene) {
         node.physicsBody?.contactTestBitMask = 1;
     }
     
     func didBeginContact(contact: SKPhysicsContact, node: SKNode) {
-        if let r = red[node], let g = green[node], let b = blue[node] {
-            red[node] = r + 0.05
-            green[node] = g + 0.1
-            blue[node] = b + 0.025
+        if let h = hue[node] {
+            hue[node] = h + 0.05
         }
         else {
-            red[node] = 0
-            green[node] = 0
-            blue[node] = 1
+            hue[node] = 0
         }
         
-        if let spriteNode = node as? SKSpriteNode, let r = red[node], let g = green[node], let b = blue[node]  {
-            if r > 0.9 {
-                red[node] = 0
+        if let spriteNode = node as? SKSpriteNode, let h = hue[node] {
+            if h > 0.9 {
+                hue[node] = 0
             }
             
-            if g > 0.9 {
-                green[node] = 0
-            }
-            
-            if b > 0.9 {
-                blue[node] = 0
-            }
-            
-            spriteNode.color = UIColor(red: r, green: g, blue: b, alpha: 1)
+            spriteNode.color = UIColor(hue: h, saturation: 1, brightness: 0.9, alpha: 1)
         }
     }
     
