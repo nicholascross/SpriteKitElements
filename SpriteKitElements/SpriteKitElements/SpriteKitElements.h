4448627fa1437ae13edf4c1284c6f7e4fe52ff45
Nicholas Cross <nicholas@nacrossweb.com>
2016-10-29 12:09:17 +1100

    Project restructure  - Created new project for framework only (further work is required to make this build for multiple platforms)  - Removed example project

---
+++ SpriteKitElements/SpriteKitElements/SpriteKitElements.h
@@ -0,0 +1,19 @@
+//
+//  SpriteKitElements.h
+//  SpriteKitElements
+//
+//  Created by Nicholas Cross on 29/10/2016.
+//  Copyright © 2016 Nicholas Cross. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SpriteKitElements.
+FOUNDATION_EXPORT double SpriteKitElementsVersionNumber;
+
+//! Project version string for SpriteKitElements.
+FOUNDATION_EXPORT const unsigned char SpriteKitElementsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SpriteKitElements/PublicHeader.h>
+
+
