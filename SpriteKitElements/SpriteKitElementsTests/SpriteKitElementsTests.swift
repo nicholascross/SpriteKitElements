1c2ae1ef88d46b56ab975c6c6d3fba6492cfa020
Nicholas Cross <nicholas@nacrossweb.com>
2016-10-30 15:34:58 +1100

    - Added some simple tests of the SpriteElement interface and SpriteElementScene

--- SpriteKitElements/SpriteKitElementsTests/SpriteKitElementsTests.swift
+++ SpriteKitElements/SpriteKitElementsTests/SpriteKitElementsTests.swift
@@ -7,30 +7,149 @@
 //
 
 import XCTest
+import SpriteKit
 @testable import SpriteKitElements
 
 class SpriteKitElementsTests: XCTestCase {
     
+    var elementScene: SpriteElementScene!
+    var node: SKNode!
+    var element: MockElement!
+    
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+        
+        elementScene = SpriteElementScene()
+        node = SKNode()
+        element = MockElement()
+        
+        elementScene.nodeInvolvedInContact = { [unowned self]
+            node, _ in
+            return node == self.node
         }
+        
+        elementScene.attachElement(element, toNode: node)
     }
     
+    func testUpdate() {
+        elementScene.update(0)
+        
+        XCTAssert(element.didUpdateElement, "Expected update element to be called")
+    }
+    
+    func testAttach() {
+        XCTAssert(element.didAttachToElement, "Expected attach element to be called")
+    }
+    
+    func testEvalActions() {
+        elementScene.didEvaluateActions()
+        
+        XCTAssert(element.didAttachToElement, "Expected didEvaluateActions to be called")
+    }
+    
+    func testSimPhysics() {
+        elementScene.didSimulatePhysics()
+        
+        XCTAssert(element.didSimulatePhysicsForElement, "Expected didSimulatePhysics to be called")
+    }
+    
+    func testApplyConstraints() {
+        elementScene.didApplyConstraints()
+        
+        XCTAssert(element.didApplyConstraintsForElement, "Expected didApplyConstraints to be called")
+    }
+    
+    func testFinishUpdate() {
+        elementScene.didFinishUpdate()
+        
+        XCTAssert(element.didFinishUpdateForElement, "Expected didFinishUpdate to be called")
+    }
+    
+    func testWillMove() {
+        elementScene.willMove(from: SKView())
+        
+        XCTAssert(element.willMoveElement, "Expected willMove(fromView) to be called")
+    }
+    
+    func testDidMove() {
+        elementScene.didMove(to: SKView())
+        
+        XCTAssert(element.didMoveElement, "Expected didMove(toView) to be called")
+    }
+    
+    func testChangeSize() {
+        elementScene.didChangeSize(CGSize(width: 0, height: 0))
+        
+        XCTAssert(element.didChangeSizeOfElement, "Expected didChangeSize to be called")
+    }
+    
+    func testBeginContact() {
+        elementScene.didBegin(SKPhysicsContact())
+        XCTAssert(element.didBeginContactForElement, "Expected didBegin to be called")
+    }
+    
+    func testEndContact() {
+        elementScene.didEnd(SKPhysicsContact())
+        
+        XCTAssert(element.didEndContactForElement, "Expected didEnd to be called")
+    }
+    
+}
+
+class MockElement : SpriteElement {
+    var didUpdateElement = false
+    var didAttachToElement = false
+    var didEvaluateActionsForElement = false
+    var didSimulatePhysicsForElement = false
+    var didApplyConstraintsForElement = false
+    var didFinishUpdateForElement = false
+    var didMoveElement = false
+    var willMoveElement = false
+    var didChangeSizeOfElement = false
+    var didBeginContactForElement = false
+    var didEndContactForElement = false
+    
+    func update(atTime currentTime: TimeInterval, delta: TimeInterval, node: SKNode) {
+        didUpdateElement = true
+    }
+    
+    func didAttach(toNode node: SKNode, inScene scene:SpriteElementScene) {
+        didAttachToElement = true
+    }
+
+    func didEvaluateActions(_ node: SKNode) {
+        didEvaluateActionsForElement = true
+    }
+    
+    func didSimulatePhysics(_ node: SKNode) {
+        didSimulatePhysicsForElement = true
+    }
+    
+    func didApplyConstraints(_ node: SKNode) {
+        didApplyConstraintsForElement = true
+    }
+    
+    func didFinishUpdate(_ node: SKNode) {
+        didFinishUpdateForElement = true
+    }
+    
+    func didMove(toView view: SKView, node: SKNode) {
+        didMoveElement = true
+    }
+    
+    func willMove(fromView view: SKView, node: SKNode) {
+        willMoveElement = true
+    }
+    
+    func didChange(size oldSize: CGSize, node: SKNode) {
+        didChangeSizeOfElement = true
+    }
+    
+    func didBegin(contact: SKPhysicsContact, node: SKNode) {
+        didBeginContactForElement = true
+    }
+    
+    func didEnd(contact: SKPhysicsContact, node: SKNode) {
+        didEndContactForElement = true
+    }
 }
